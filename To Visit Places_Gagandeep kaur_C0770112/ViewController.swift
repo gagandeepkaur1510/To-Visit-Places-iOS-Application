49d9ac7319a9c5065331b294d102fe20d06adcca
gagandeepkaur1510 <gagandeepkaur1510@gmail.com>
2020-06-14 19:47:58 -0400

    Initial Commit

---
+++ To Visit Places_Gagandeep kaur_C0770112/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Visit Places_Gagandeep kaur_C0770112
+//
+//  Created by Mac on 6/14/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
